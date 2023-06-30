68e7551d8e5b47170213b3739d4ee55ada2ffe1e
nuttdguy <pygnasakdev@gmail.com>
2023-06-30 16:00:50 -0500

    fix conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './pages/profile/profile.component';
+// import { PostsComponent } from './pages/posts/posts.component';
+// import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RouteguardService } from './services/routeguard.service';
 
 const routes: Routes = [
+  // { path: 'posts/create', component: CreatePostComponent },
+
   // {path: '', component: },
   // {path: 'register', component: },
   // {path: 'login', component: },
-   {path: 'profile', component: ProfileComponent, canActivate: [RouteguardService]},
-   {path: '**', component: NotfoundComponent},
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    // canActivate: [RouteguardService],
+  },
+  {
+    path: 'posts',
+    // component: PostsComponent,
+  },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
