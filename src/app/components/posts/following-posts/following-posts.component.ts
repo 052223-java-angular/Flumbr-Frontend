c3ccd65ad5b28a666f7da2588ea5a8a6e3413d4a
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-07-11 12:16:37 -0400

    Refresh posts on update/create for trending and following

--- src/app/components/posts/following-posts/following-posts.component.ts
+++ src/app/components/posts/following-posts/following-posts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgEventBus } from 'ng-event-bus';
 import { Subscription } from 'rxjs';
+import { EventBusEvents } from 'src/app/global/event-bus-events';
 import { PostRes } from 'src/app/models/post/post';
 import { FollowService } from 'src/app/services/follow/follow.service';
 import { PostService } from 'src/app/services/post/post.service';
@@ -13,11 +15,17 @@ export class FollowingPostsComponent implements OnInit, OnDestroy {
   posts: Array<PostRes> = [];
   isLoading = false;
   followingSubscription!: Subscription;
+  postSub: Subscription;
 
   constructor(
     private postService: PostService,
-    private followService: FollowService
-  ) {}
+    private followService: FollowService,
+    private eventBus: NgEventBus
+  ) {
+    this.postSub = this.eventBus.on(`${EventBusEvents.POST}*`).subscribe(() => {
+      this.getPosts(1);
+    });
+  }
 
   ngOnInit(): void {
     this.getPosts(1);
