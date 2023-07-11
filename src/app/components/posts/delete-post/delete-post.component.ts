5e1d34078e18516bfcdf8ac6058a81785c306ff1
Patrick Powers <patrickpowers221@gmail.com>
2023-07-11 13:05:44 -0400

    Got the delete posts working, should be no issues

---
+++ src/app/components/posts/delete-post/delete-post.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { PostRes } from 'src/app/models/post/post';
+import { PostService } from 'src/app/services/post/post.service';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-delete-post',
+  templateUrl: './delete-post.component.html',
+  styleUrls: ['./delete-post.component.scss']
+})
+export class DeletePostComponent {
+   @Input()
+   post!: PostRes
+
+   constructor(private postservice: PostService, private messageservice: MessageService){}
+
+   deletePost(post_id:string)
+   {
+      console.log("in deletePost method");
+      this.postservice.deletePost(post_id).subscribe({
+          next: (val) => {
+            console.log(val);
+            this.messageservice.add({
+                severity: "success",
+                summary: "Post Deleted",
+                detail: "Your post has been deleted"
+            })
+            
+          },
+          error: (err) => {
+              console.log(err);
+          }
+      })
+   }
+}
