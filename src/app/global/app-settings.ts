5df63dc40bd69ee49c870c006509a035e138dcbf
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-07-11 13:34:18 -0700

    Fix sharelink and post actions

--- src/app/global/app-settings.ts
+++ src/app/global/app-settings.ts
@@ -2,7 +2,7 @@ import { environment } from '../../environments/environment';
 import { environment as envDev } from 'src/environments/environment.development';
 
 export class AppSettings {
-  public static API_URL = 'http://localhost:5000/flumblr/api';
+  public static API_URL = environment.apiBaseUrl;
   public static DEFAULT_MESSAGE_LIFE = 3000;
   public static BYTES_PER_MEGABYTE = 1048576;
   public static CREATE_POST_SIZE_LIMIT = 128 * this.BYTES_PER_MEGABYTE;
