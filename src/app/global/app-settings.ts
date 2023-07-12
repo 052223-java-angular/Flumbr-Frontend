53aa373b79d4669ea16a6a147ec1a75f2c181e10
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-07-12 11:17:24 -0400

    Fix environment

--- src/app/global/app-settings.ts
+++ src/app/global/app-settings.ts
@@ -1,11 +1,10 @@
 import { environment } from '../../environments/environment';
-import { environment as envDev } from 'src/environments/environment.development';
 
 export class AppSettings {
   public static API_URL = environment.apiBaseUrl;
   public static DEFAULT_MESSAGE_LIFE = 3000;
   public static BYTES_PER_MEGABYTE = 1048576;
   public static CREATE_POST_SIZE_LIMIT = 128 * this.BYTES_PER_MEGABYTE;
-  public static GIPHY_API_URL = envDev.giphyApiBaseUrl;
-  public static GIPHY_API_KEY = envDev.giphyApiKey;
+  public static GIPHY_API_URL = environment.giphyApiBaseUrl;
+  public static GIPHY_API_KEY = environment.giphyApiKey;
 }
